212d94a42c4e7fa7e5cd31b338b0b4f80746c22d
吉田裕哉 <yuu@yoshi.local>
2023-03-09 15:25:54 +0900

    create sitemap

---
+++ app/out/_next/static/M2e0kCXWS0YAQOIMagPmy/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fbooks\u002F[id]","\u002Fevents\u002F[id]","\u002Fnews\u002F[id]","\u002Fnotification\u002F[id]","\u002Fprofile"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
